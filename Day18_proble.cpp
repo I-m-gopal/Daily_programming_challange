1e50a8c49d02e7747fbafec437dd21bdb753a8ea
I-m-gopal <gopalmandol439@gmail.com>
2024-09-26 23:55:04 +0530

    Create Day18_proble.cpp

---
+++ Day18_proble.cpp
@@ -0,0 +1,23 @@
+//NUmber of factors
+#include <iostream>
+#include <cmath>
+using namespace std;
+int countDivisors(int N) {
+    int count = 0;
+    for (int i = 1; i * i <= N; i++) {
+        if (N % i == 0) {
+            count++;
+            if (i != N / i) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << countDivisors(N) << endl;
+    return 0;
+}
